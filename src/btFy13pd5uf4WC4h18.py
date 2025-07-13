a8dd91ef01abd2fcda5e8098af9a5fa3c894959d
KSSW <76238373+KSSW@users.noreply.github.com>
2025-07-13 10:58:48 +0800

    Delete src directory

--- src/btFy13pd5uf4WC4h18.py
+++
@@ -1,9 +0,0 @@
-#! /usr/bin/env python
-# -*- coding: utf-8 -*-
-
-import os
-import sys
-
-def lSfgApatkdxsVcGcrktoFd():
-    if os.getenv('PERSIA') != '1':
-        sys.exit(1)
--- src/ecq.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-
-def aut():
-    encoded = "dHNTQ0IgSU5GTyA6IHRzU0NCIHYwLjAuMSwgKGMpIEtTU1cg"
-    return encoded
