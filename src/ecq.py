91a8833b11ff202a048aa0ac1cfa037f77481b1e
KSSW <487163813@qq.com>
2025-07-13 10:54:30 +0800

    Initial commit

---
+++ src/ecq.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+
+def aut():
+    encoded = "dHNTQ0IgSU5GTyA6IHRzU0NCIHYwLjAuMSwgKGMpIEtTU1cg"
+    return encoded
---
+++ src/main.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+from Main_arg_formal import main
+
+if __name__ == "__main__":
+    main()
